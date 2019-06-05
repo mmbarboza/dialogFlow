6e5b148503aa6a9150cfaf593cdcb21c8683cd5f
Monica Barboza <barboza.monica2@gmail.com>
2019-06-04 19:56:24 -0500

    form rendering

--- client/components/App.js
+++ client/components/App.js
@@ -1,15 +1,36 @@
 import React from 'react';
+import BotUI from 'botui';
 
 
 
 export default class ChatBot extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      userMessage: '',
+      conversation: [],
+    };
+  }
 
+  handleChange(event){
+    event.preventDefault();
+    this.setState({userMessage: event.target.value});
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    this.setState({
+      conversation: [...this.state.conversation, this.state.userMessage]
+    })
+  }
 
   render(){
-    return(
+
+    return (
       <div>
         <form onSubmit={this.handleSubmit}>
-        <input
+
+          <input value = {this.state.userMessage} onChange={this.handleChange}/>
         </form>
       </div>
     );
--- client/index.js
+++ client/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ChatBot from './components/App';
 
 console.log("in index.js")
 ReactDOM.render(
 
-  <div>Hello, world!</div>,
-
+  // <div>Hello, world!</div>,
+<ChatBot />,
   document.getElementById('app') // make sure this is the same as the id of the div in your index.html
 );
