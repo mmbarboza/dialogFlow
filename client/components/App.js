6b47e34594fb8eb01c4c56db264834e8216028a9
Monica Barboza <barboza.monica2@gmail.com>
2019-06-06 15:14:40 -0500

    smallTalk completed

--- client/components/App.js
+++ client/components/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import BotUI from 'botui';
-require("@babel/polyfill");
+require('@babel/polyfill');
 import axios from 'axios';
-
-
 
 
 export default class ChatBot extends React.Component{
@@ -24,28 +21,58 @@ export default class ChatBot extends React.Component{
 
   async handleSubmit(event){
     event.preventDefault();
+    //add user's message to the conversation
     this.setState({
-      conversation: [...this.state.conversation, this.state.userMessage]
-    })
+      conversation: [...this.state.conversation, {
+        text: this.state.userMessage,
+        user: 'human'
+      }]
+    });
     const chatReply = await axios.post(`/runSample/${this.state.userMessage}`);
-    console.log("user message", this.state.userMessage)
-    console.log("robot response", chatReply)
+    console.log('user message', this.state.userMessage);
+    console.log('robot response', chatReply);
+    //add the chatbot's response to the conversation
     this.setState({
-      conversation: [...this.state.conversation, chatReply.data],
+      conversation: [...this.state.conversation, {
+        text: chatReply.data,
+        user: 'ai'
+      }],
       userMessage: ''
-    })
+    });
   }
 
   render(){
+  const ChatBubble = (text, i, className) => {
+    return (
+      <div key={`${className}-${i}`} className={`${className} chat-bubble`}>
+        <span className="chat-content">{text}</span>
+      </div>
+    );
+  };
+
+  const chat = this.state.conversation.map((message, index) =>
+    ChatBubble(message.text, index, message.user)
+  );
 
     return (
-      <div>
-        <div>{this.state.conversation}</div>
-        <form onSubmit={this.handleSubmit}>
-
-          <input type="text" value = {this.state.userMessage} onChange={this.handleChange} placeholder="Type your message and hit enter" />
-        </form>
+    <div>
+      <h1>SmallTalk Chatbot</h1>
+      <div className="chat-window">
+       <div className="conversation-view">{chat}</div>
+        <div className="message-box">
+          <form onSubmit={this.handleSubmit}>
+             <input
+              value={this.state.userMessage}
+              onChange={this.handleChange}
+              className="text-input"
+              type="text"
+              autoFocus
+              placeholder="Type your message and hit enter"
+            />
+          </form>
+        </div>
       </div>
+    </div>
     );
   }
 }
